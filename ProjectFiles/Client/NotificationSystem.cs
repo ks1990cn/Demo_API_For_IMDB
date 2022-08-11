66e9e7f694facb9206f76882b065cd4088343d68
Tanmay Seth <kushal.seth2010@gmail.com>
2022-08-11 17:29:36 +0530

    Parallelly take input and check for notification too

--- ProjectFiles/Client/NotificationSystem.cs
+++ ProjectFiles/Client/NotificationSystem.cs
@@ -13,23 +13,21 @@ namespace Client
         static string prevResponse = "";
         public static async Task KeepCheckingRequests()
         {
-            label:
-
-            var pingDuration = TimeSpan.FromSeconds(10);
-
-            string responseBody = await Client.GetStringAsync("https://localhost:44337/GetMovie");
-
-            if (prevResponse != responseBody)
+            while (true)
             {
-                Console.Clear();
-                Console.WriteLine(responseBody);
+                var pingDuration = TimeSpan.FromSeconds(10);
+
+                string responseBody = await Client.GetStringAsync("https://localhost:44337/GetMovie");
+
+                if (prevResponse != responseBody)
+                {
+                    Console.Clear();
+                    Console.WriteLine(responseBody);
+                }
+                prevResponse = responseBody;
+
+                Thread.Sleep(TimeSpan.FromSeconds(5));
             }
-            prevResponse = responseBody;
-
-            Thread.Sleep(TimeSpan.FromSeconds(5));
-
-            goto label;
-            
         }
     }
 }
--- ProjectFiles/Client/Program.cs
+++ ProjectFiles/Client/Program.cs
@@ -6,11 +6,23 @@ namespace Client
 {
     class Program : ClientBaseClass
     {
+        static async Task Input()
+        {
+            var bakwas = await Console.In.ReadLineAsync();
+        }
         static async Task Main(string[] args)
         {
             try
             {
                 await NotificationSystem.KeepCheckingRequests();
+                
+                //By below manner one can take input and parallely fetch output
+                
+                //Parallel.Invoke(async () =>
+                //{
+                //await NotificationSystem.KeepCheckingRequests();
+
+                //}, async () => { await Input(); });
             }
             catch (Exception)
             {
